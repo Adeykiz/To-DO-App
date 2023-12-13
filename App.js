a27c65ef402e1ae3e1a8bc6b3b1fffe7127049ec
Adeykiz <93167459+Adeykiz@users.noreply.github.com>
2023-12-13 16:54:47 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import { SafeAreaView, StyleSheet, View } from 'react-native';
+import TodoScreen from './src/screen/TodoScreen';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <View style={styles.container}>
+        <TodoScreen />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
